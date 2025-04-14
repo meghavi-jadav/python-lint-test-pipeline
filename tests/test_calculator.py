86c16a94ee5db46222a6cc4b777ca5027c2dfce6
Meghavi Jadav <mjadav@parkar.digital>
2025-04-14 14:24:03 +0530

    Update test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,21 +3,20 @@ from app.calculator import Calculator
 
 @pytest.fixture
 def calc():
-  return Calculator()
+    return Calculator()
 
 def test_add(calc):
-  assert calc.add(10, 5) == 15
+    assert calc.add(10, 5) == 15
 
-def test_substract(calc):
-  assert calc.subtract(10, 5) == 5
-  
+def test_subtract(calc):  
+    assert calc.subtract(10, 5) == 5
+
 def test_multiply(calc):
-  asserct calc.multiply(10, 5) == 50
+    assert calc.multiply(10, 5) == 50
 
 def test_divide(calc):
-  assert calc.divide(10, 2) == 5.0
+    assert calc.divide(10, 2) == 5.0
 
 def test_divide_by_zero(calc):
-  with pytest.raises(ValueError, match="Cannot divide by zero"):
-    calc.divide(10, 0)
-  
+    with pytest.raises(ValueError, match="Cannot divide by zero"):
+        calc.divide(10, 0)
